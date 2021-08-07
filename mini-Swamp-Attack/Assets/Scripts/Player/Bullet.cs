14875d36f0a92a84c9c746c541a026a905e5410e
Максим Майнагашев <maxuser99@mail.ru>
2021-08-07 17:54:16 +0700

    Scripts for Weapons and bullets added
    LMB instantiate bullet

---
+++ mini-Swamp-Attack/Assets/Scripts/Player/Bullet.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private int _damage;
+    [SerializeField] private float _speed;
+    
+    
+    void Update()
+    {
+        transform.Translate(Vector2.left * _speed * Time.deltaTime, Space.World);    
+    }
+    
+}
---
+++ mini-Swamp-Attack/Assets/Scripts/Player/Weapons/Pistol.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : Weapon
+{
+    public override void Shoot(Transform shootPoint)
+    {
+        Instantiate(Bullet, shootPoint.position, Quaternion.identity);
+    }
+    
+void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
